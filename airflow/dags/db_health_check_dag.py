47c5556204b4840d2fa56fc1fa40cefc7225816d
Jay-Dee <johnsondesouza@gmail.com>
2026-05-22 16:17:09 +0100

    feat: add dag to check db connectivity

---
+++ airflow/dags/db_health_check_dag.py
@@ -0,0 +1,29 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime, timedelta
+
+from lib.db.postgres_db_health import postgres_health_check
+
+
+default_args = {
+    "owner": "lcip",
+    "retries": 1,
+    "retry_delay": timedelta(seconds=10),
+}
+
+with DAG(
+    dag_id="postgres_health_dag",
+    description="LCIP Postgres health check DAG",
+    default_args=default_args,
+    start_date=datetime(2024, 1, 1),
+    schedule_interval="*/1 * * * *",  # every minute
+    catchup=False,
+    tags=["lcip", "health", "postgres"],
+) as dag:
+
+    check_db = PythonOperator(
+        task_id="postgres_health_check",
+        python_callable=postgres_health_check,
+    )
+
+    check_db
